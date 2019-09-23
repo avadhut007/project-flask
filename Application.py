3a0aaacd0b1df89245a9199bec69c1e374d2559b
Avadhut Talbar <avadhut.t@afourtech.pune>
2019-09-23 12:08:51 +0530

    "added new files"

---
+++ Application.py
@@ -0,0 +1,33 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+posts = [
+
+    {
+        'author':'Tony Stark',
+        'title':'Blog Post 1',
+        'content':'First post content',
+        'date_posted':'September 20, 2019'
+    },
+
+    {
+        'author':'Steve Rogers',
+        'title':'Blog Post 2',
+        'content':'Second post content',
+        'date_posted':'September 21, 2019'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+
+def about():
+    return render_template('about.html', title='About')
+
+if __name__ == "__main__":
+    app.run(debug=True)    
