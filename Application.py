fde192531e50d22e9cfe1a1d53f0cf98f482ae1e
Avadhut Talbar <avadhut.t@AFOURTECH.PUNE>
2019-09-25 13:07:53 +0530

    Added Forms

--- Application.py
+++ Application.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+#secrets.token_hex(16)
+app.config('SECRET_KEY'='89411675b09c95600928be84bf9cc0ec')
 
 posts = [
 
@@ -25,9 +29,20 @@ def home():
     return render_template('home.html', posts=posts)
 
 @app.route("/about")
-
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def register():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+    
+
 
 if __name__ == "__main__":
     app.run(debug=True)    
